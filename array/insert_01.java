8ef16d9532935453d254428b75b03046aefae644
ogsahil <contactsahilpnayak@gmail.com>
2023-12-31 00:37:46 +0530

    array insertion operation

---
+++ array/insert_01.java
@@ -0,0 +1,14 @@
+public class insert_01 {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 20;
+        arr[1] = 10;
+        arr[2] = 30;
+        arr[3] = 40;
+        System.out.println("this is on index 0\t" + arr[0]);
+        System.out.println("this is on index 1\t" + arr[1]);
+        System.out.println("this is on index 2\t" + arr[2]);
+        System.out.println("this is on index 3\t" + arr[3]);
+
+    }
+}
